69691afa68af63ba06ec0478d0c6fc660774fcf5
Fabian Fajardo <fabianjff97@gmail.com>
2023-03-08 11:01:35 -0500

    VF

--- programa.js
+++ programa.js
@@ -1,9 +1,15 @@
-function media(){
+function nota(){
  let x= parseInt(document.getElementById("x").value);
  let y= parseInt(document.getElementById("y").value);
  let z= parseInt(document.getElementById("z").value);
- let promedio= ((x+y+z)/3);
- document.writeln("La Media es: "+ promedio);
+ let e= parseInt(document.getElementById("examen_f").value);
+ let n= parseInt(document.getElementById("trabajo_f").value);
+ let ef= (e*0.3);
+ let nf= (n*0.15);
+ let promedio= (((x+y+z)/3)*0.55);
+ let final= ef+nf+promedio;
+ document.writeln("La Nota Final es: " + final );
+
 
  
 
