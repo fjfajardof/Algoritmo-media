4dd149cdf831a47fb749145e0aabfdf30461af55
Fabian Fajardo <fabianjff97@gmail.com>
2023-03-08 02:11:23 -0500

    v1.0

---
+++ programa.js
@@ -0,0 +1,10 @@
+function media(){
+ let x= parseInt(document.getElementById("x").value);
+ let y= parseInt(document.getElementById("y").value);
+ let z= parseInt(document.getElementById("z").value);
+ let promedio= ((x+y+z)/3);
+ document.writeln("La Media es: "+ promedio);
+
+ 
+
+}
